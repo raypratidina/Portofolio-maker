14f63bd91fe6122ccb5caccb47819af634241f5a
raypratidina <Raypz642@gmail.com>
2025-12-06 11:37:01 +0700

    fix: Build error middleware export

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,11 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+    callbacks: {
+        authorized: ({ token }) => !!token,
+    },
+});
 
 export const config = {
-    matcher: ["/admin/dashboard/:path*", "/admin/settings/:path*", "/admin/projects/:path*"]
+    matcher: ["/admin/dashboard/:path*", "/admin/settings/:path*", "/admin/projects/:path*"],
 };
